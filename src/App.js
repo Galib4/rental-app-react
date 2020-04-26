b70e107db10eb2f446e911f851a2eae604374533
Galib4 <54716504+Galib4@users.noreply.github.com>
2020-04-26 12:17:25 +0530

    Added Rental cards and its styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Header } from './shared/Header';
+import { RentalCard } from './components/rental/RentalCard';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <div className='container'>
+      <section id='rentalListing'>
+        <h1 className='page-title'>Your Home All Around the World</h1>
+        <div className='row'>
+          <RentalCard />
+          <RentalCard />
+          <RentalCard />
+          <RentalCard />
+        </div>
+      </section>
+</div>
     </div>
     
   );
